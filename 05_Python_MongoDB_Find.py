fbbda0a8734f2aa15a25105c0b7db992977125c1
Pedro Carneiro Jr <pedrokarneiro.bsa@gmail.com>
2021-05-07 16:10:48 -0300

    More files

---
+++ 05_Python_MongoDB_Find.py
@@ -0,0 +1,94 @@
+# 05_Python_MongoDB_Find.py
+# 
+# Python MongoDB Find
+# ===================
+# 
+# In MongoDB we use the find() and find_one() methods to find data in a collection
+# just like the SELECT statement is used to find data in a table in a MySQL database.
+# 
+# Find One
+# --------
+# To select data from a collection in MongoDB, we can use the find_one() method.
+# 
+# The find_one() method returns the first occurrence in the selection.
+# 
+# Example
+# Find the first document in the customers collection:
+# 
+# import pymongo
+# 
+# myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+# mydb = myclient["mydatabase"]
+# mycollection = mydb["customers"]
+# 
+# x = mycollection.find_one()
+# 
+# print(x)
+# 
+# Find All
+# --------
+# To select data from a table in MongoDB, we can also use the find() method.
+# 
+# The find() method returns all occurrences in the selection.
+# 
+# The first parameter of the find() method is a query object.
+# In this example we use an empty query object, which selects all documents in the collection.
+# 
+# No parameters in the find() method gives you the same result as SELECT * in MySQL.
+# 
+# Example
+# Return all documents in the "customers" collection, and print each document:
+# 
+# import pymongo
+# 
+# myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+# mydb = myclient["mydatabase"]
+# mycollection = mydb["customers"]
+# 
+# for x in mycollection.find():
+#   print(x)
+# 
+# Return Only Some Fields
+# -----------------------
+# The second parameter of the find() method is an object describing which fields to include in the result.
+# 
+# This parameter is optional, and if omitted, all fields will be included in the result.
+# 
+# Example
+# Return only the names and addresses, not the _ids:
+# 
+# import pymongo
+# 
+# myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+# mydb = myclient["mydatabase"]
+# mycollection = mydb["customers"]
+# 
+# for x in mycollection.find({},{ "_id": 0, "name": 1, "address": 1 }):
+#   print(x)
+# 
+# You are not allowed to specify both 0 and 1 values in the same object (except if one of the fields is the _id field).
+# If you specify a field with the value 0, all other fields get the value 1, and vice versa:
+# 
+# Example
+# This example will exclude "address" from the result:
+# 
+# import pymongo
+# 
+# myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+# mydb = myclient["mydatabase"]
+# mycollection = mydb["customers"]
+# 
+# for x in mycollection.find({},{ "address": 0 }):
+#   print(x)
+# 
+# Example
+# You get an error if you specify both 0 and 1 values in the same object (except if one of the fields is the _id field):
+# 
+# import pymongo
+# 
+# myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+# mydb = myclient["mydatabase"]
+# mycollection = mydb["customers"]
+# 
+# for x in mycollection.find({},{ "name": 1, "address": 0 }):
+#   print(x)
