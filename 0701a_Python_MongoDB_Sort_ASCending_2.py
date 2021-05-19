7c537c8f750a9b3504bd427332c1408beab7f0c5
Pedro Carneiro Jr <pedrokarneiro.bsa@gmail.com>
2021-05-19 10:55:47 -0300

    Sort

---
+++ 0701a_Python_MongoDB_Sort_ASCending_2.py
@@ -0,0 +1,25 @@
+# Python MongoDB Sort
+# 0701_Python_MongoDB_Sort_ASCending.py
+#
+# Sort the Result
+# ===============
+# Use the sort() method to sort the result in ascending or descending order.
+# The sort() method takes one parameter for "fieldname" and one parameter for "direction" (ascending is the default direction).
+# 
+# Example: Sort the result alphabetically by name
+
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["mydatabase"]
+mycoll = mydb["customers"]
+
+# sort("name")     #ASCending (default)
+# sort("name", 1)  #ASCending <-------- In this example
+# sort("name", -1) #DESCending
+mydocs = mycoll.find().sort("name", 1)
+
+for document in mydocs:
+    print(document)
+
+
---
+++ 07_Python_MongoDB_Sort.py
@@ -0,0 +1,44 @@
+# Python MongoDB Sort
+# 07_Python_MongoDB_Sort.py
+#
+# Sort the Result
+# ===============
+# Use the sort() method to sort the result in ascending or descending order.
+# The sort() method takes one parameter for "fieldname" and one parameter for "direction" (ascending is the default direction).
+# 
+# Example: Sort the result alphabetically by name
+# 
+# import pymongo
+# 
+# myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+# mydb = myclient["mydatabase"]
+# mycoll = mydb["customers"]
+# 
+# mydocs = mycoll.find().sort("name")
+# 
+# for document in mydocs:
+#     print(document)
+# 
+# #### END ####
+# 
+# Sort Descending
+# ---------------
+# Use the value -1 as the second parameter to sort descending.
+# 
+# sort("name", 1)  #ascending
+# sort("name", -1) #descending
+# 
+# Example: Sort the result reverse alphabetically by name
+# 
+# import pymongo
+# 
+# myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+# mydb = myclient["mydatabase"]
+# mycoll = mydb["customers"]
+# 
+# mydocs = mycoll.find().sort("name", -1)
+# 
+# for document in mydocs:
+#     print(document)
+# 
+# #### END ####
